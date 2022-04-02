2bd54dd879da16aaf7441c4e1a91ac07222621bf
arp <arp@akamai.com>
2022-04-02 18:38:15 +0530

    Append link to screener tab , rename variable and color change

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -2,20 +2,33 @@ var watchListLoadWatcher = setInterval (checkForWatchlistLoad, 1000);
 var totalTries = 0;
 
 function checkForWatchlistLoad() {
-    x = $("*[data-symbol-short]");
+    watchListResults = $("*[data-symbol-short]");
+    screenerResults = $(".tv-screener__symbol.tv-screener__symbol--text.apply-common-tooltip");
+    
     totalTries += 1;
     if(totalTries > 15){
         clearInterval (watchListLoadWatcher);
         console.log("No Watchlist found in the past "+totalTries+" seconds.");
     }
-    if (x.length!=0) {
+    if (watchListResults.length!=0) {
         clearInterval (watchListLoadWatcher);
-        for(i=0;i<x.length;i++){
-            sym = $(x[i]).children().eq(1).children().children().eq(1).text();
+        for(i=0;i<watchListResults.length;i++){
+            watchList = $(watchListResults[i]);
+            sym = watchList.children().eq(1).children().children().eq(1).text();
             screener_link = "https://www.screener.in/company/"+sym+"/";
             marketsmith_link = "https://marketsmithindia.com/mstool/eval/"+sym+"/evaluation.jsp#/";
-            $(x[i]).children().eq(2).after('&nbsp; <a href="'+screener_link+'", style="margin-left=4px;" target="_blank">S</a>');
-            $(x[i]).children().eq(2).after('&nbsp; <a href="'+marketsmith_link+'", style="margin-left=4px;" target="_blank">M</a>');
+            watchList.children().eq(2).after('&nbsp; <a href="'+screener_link+'", style="margin-left=4px;color:red;" target="_blank">S</a>');
+            watchList.children().eq(2).after('&nbsp; <a href="'+marketsmith_link+'", style="margin-left=4px;color:green;" target="_blank">M</a>');
         }
     }
+
+    for(i=0;i<screenerResults.length;i++){
+        row = $(screenerResults[i]);
+        sym = row.text();
+        screener_link = "https://www.screener.in/company/" + sym + "/";
+        marketsmith_link = "https://marketsmithindia.com/mstool/eval/" + sym + "/evaluation.jsp#/";
+        $(row).after('&nbsp; <a href="' + screener_link + '", style="margin-left=4px;color:red;" target="_blank">S</a>');
+        $(row).after('&nbsp; <a href="' + marketsmith_link + '", style="margin-left=4px;color:green;" target="_blank">M</a>');
+    }
+
 }
